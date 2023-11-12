468bf8e29750a90d4405e44eb60ef7d54ce5cd4e
MdZahidulIslam88 <68554645+MdZahidulIslam88@users.noreply.github.com>
2023-11-11 23:00:37 -0600

    js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+  let typed = new Typed('.home-text44',{
+    strings: ['web designer!','web developer!','Freelancer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 2000,
+    loop: true,
+  });
